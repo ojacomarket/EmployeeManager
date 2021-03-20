b43c0139a796067ccfe8eae0b6d4df1eed641780
Artjom Makarov <armaka@ttu.ee>
2021-03-20 23:20:19 +0200

    Add annotation to indicate, that value for primary keys (@Id) are taken from db id column

--- src/main/java/com/reddit/clone/model/Post.java
+++ src/main/java/com/reddit/clone/model/Post.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
@@ -15,5 +17,6 @@ import javax.persistence.Id;
 @NoArgsConstructor
 public class Post {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long postId;
 }
