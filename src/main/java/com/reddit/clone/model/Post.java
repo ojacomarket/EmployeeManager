e8cad7db973f07008f77f1b8fc593fe7f25445af
Artjom Makarov <armaka@ttu.ee>
2021-03-20 23:13:16 +0200

    Add constructor without params, needed for Database interaction

--- src/main/java/com/reddit/clone/model/Post.java
+++ src/main/java/com/reddit/clone/model/Post.java
@@ -3,6 +3,7 @@ package com.reddit.clone.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 
@@ -10,5 +11,6 @@ import javax.persistence.Entity;
 @Entity
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class Post {
 }
