11ddb217e1c945ebdc0d4e6c2aa158b5f48d2335
Artjom Makarov <armaka@ttu.ee>
2021-03-21 11:21:55 +0200

    Add @Service annot. everytime you have Service class

--- src/main/java/com/reddit/clone/service/EmployeeService.java
+++ src/main/java/com/reddit/clone/service/EmployeeService.java
@@ -1,7 +1,9 @@
 package com.reddit.clone.service;
 
 import com.reddit.clone.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
 
