f235dea605b13f98b1d610f33a4bca4f7a67bbf5
Artjom Makarov <armaka@ttu.ee>
2021-03-21 11:25:19 +0200

    Add method declaration to be able add employee into database

--- src/main/java/com/reddit/clone/service/EmployeeService.java
+++ src/main/java/com/reddit/clone/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.reddit.clone.service;
 
+import com.reddit.clone.model.Employee;
 import com.reddit.clone.repository.EmployeeRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,7 @@ import org.springframework.stereotype.Service;
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
 
+    public Employee addNewEmployee (Employee employeeToBeAddedIntoDb) {
+        
+    }
 }
