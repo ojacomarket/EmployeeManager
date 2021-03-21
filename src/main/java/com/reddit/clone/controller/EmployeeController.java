8dc4076b23a3ace5a1908f557003b4ea113a29f1
Artjom Makarov <armaka@ttu.ee>
2021-03-21 14:35:13 +0200

    Add @RequestMapping as BASE URL for the whole application --> STARTING POINT

--- src/main/java/com/reddit/clone/controller/EmployeeController.java
+++ src/main/java/com/reddit/clone/controller/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.reddit.clone.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController {
 }
