30615c90a2186d0d57221be1a766b137f111365d
Artjom Makarov <armaka@ttu.ee>
2021-03-21 11:14:47 +0200

    Extend from JpaRepository to open functionality to access db

--- src/main/java/com/reddit/clone/repository/EmployeeRepository.java
+++ src/main/java/com/reddit/clone/repository/EmployeeRepository.java
@@ -1,4 +1,10 @@
 package com.reddit.clone.repository;
 
-public interface EmployeeRepository {
+import com.reddit.clone.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * JpaRepository<Entity, Primary Key data type>
+ */
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
