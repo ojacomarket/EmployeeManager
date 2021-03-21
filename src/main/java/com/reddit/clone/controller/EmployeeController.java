5a033643210e8ff528b719910bb751044c09edb9
Artjom Makarov <armaka@ttu.ee>
2021-03-21 14:38:12 +0200

    Inject Service via @AllArgsConstructor

--- src/main/java/com/reddit/clone/controller/EmployeeController.java
+++ src/main/java/com/reddit/clone/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.reddit.clone.controller;
 
 import com.reddit.clone.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/employee")
+@AllArgsConstructor
 public class EmployeeController {
     private final EmployeeService employeeService;
 }
