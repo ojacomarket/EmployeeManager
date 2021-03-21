eba9369eb2338be86e0efc8f20a2e0a60e73a3ff
Artjom Makarov <armaka@ttu.ee>
2021-03-21 10:00:52 +0200

    Add initial field

--- src/main/java/com/reddit/clone/model/Employee.java
+++ src/main/java/com/reddit/clone/model/Employee.java
@@ -1,4 +1,12 @@
 package com.reddit.clone.model;
 
 public class Employee {
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imgURL;
+    private String employeeCode;
+
 }
