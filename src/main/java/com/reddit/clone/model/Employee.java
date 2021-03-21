38afdea6b3df4a5218e27e50edc6830be561fbe2
Artjom Makarov <armaka@ttu.ee>
2021-03-21 10:05:54 +0200

    Add @Id to indicate PRIMARY KEY of field

--- src/main/java/com/reddit/clone/model/Employee.java
+++ src/main/java/com/reddit/clone/model/Employee.java
@@ -1,10 +1,12 @@
 package com.reddit.clone.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Entity
 public class Employee implements Serializable {
+    @Id
     private Long id;
     private String name;
     private String email;
