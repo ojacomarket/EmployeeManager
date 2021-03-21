9d9054aa40b9d75ba44061cfad659dd94c0cfa2f
Artjom Makarov <armaka@ttu.ee>
2021-03-21 14:37:16 +0200

    Add reference to Service

--- src/main/java/com/reddit/clone/controller/EmployeeController.java
+++ src/main/java/com/reddit/clone/controller/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.reddit.clone.controller;
 
+import com.reddit.clone.service.EmployeeService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
+    private final EmployeeService employeeService;
 }
