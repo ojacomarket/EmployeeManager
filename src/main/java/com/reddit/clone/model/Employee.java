c8a1901fa82f5ec78ae49a1cb5cbd76b3ffd7c97
Artjom Makarov <armaka@ttu.ee>
2021-03-21 10:12:04 +0200

    Add empty constructor

--- src/main/java/com/reddit/clone/model/Employee.java
+++ src/main/java/com/reddit/clone/model/Employee.java
@@ -2,6 +2,7 @@ package com.reddit.clone.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -9,6 +10,7 @@ import java.io.Serializable;
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Employee implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
