a6d93523c7df5abfc032a1974a31d021cebb1c1f
Artjom Makarov <armaka@ttu.ee>
2021-03-21 10:03:50 +0200

    Make class implements Serializable and a bit theory behind that decision

--- src/main/java/com/reddit/clone/model/Employee.java
+++ src/main/java/com/reddit/clone/model/Employee.java
@@ -1,6 +1,13 @@
 package com.reddit.clone.model;
 
-public class Employee {
+import java.io.Serializable;
+
+/**
+ * Why we use "implements Serializable" here? Is because our Employee class will be used as Entity in
+ * Database, therefore we have to pass back and forth around our application and it is much easier
+ * to be done via Stream API. So Serializable makes a possibility for a class to act as Stream also.
+ */
+public class Employee implements Serializable {
     private Long id;
     private String name;
     private String email;
