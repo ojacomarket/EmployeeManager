8d64007ca384de1d53e1deab2b6e24f11660f197
Artjom Makarov <armaka@ttu.ee>
2021-03-21 10:05:19 +0200

    Add @Entity on class, to make it possible to insert into db

--- src/main/java/com/reddit/clone/model/Employee.java
+++ src/main/java/com/reddit/clone/model/Employee.java
@@ -1,12 +1,9 @@
 package com.reddit.clone.model;
 
+import javax.persistence.Entity;
 import java.io.Serializable;
 
-/**
- * Why we use "implements Serializable" here? Is because our Employee class will be used as Entity in
- * Database, therefore we have to pass back and forth around our application and it is much easier
- * to be done via Stream API. So Serializable makes a possibility for a class to act as Stream also.
- */
+@Entity
 public class Employee implements Serializable {
     private Long id;
     private String name;
