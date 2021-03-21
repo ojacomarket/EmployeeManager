23fda3f8f7ff5ad2b957a7596a2796a1e80dd355
Artjom Makarov <armaka@ttu.ee>
2021-03-21 11:22:39 +0200

    private final variable SHOULD have been initialized at compile time, thats why we instantiate it using Lombok @AlllArgsConstructor

--- src/main/java/com/reddit/clone/service/EmployeeService.java
+++ src/main/java/com/reddit/clone/service/EmployeeService.java
@@ -1,9 +1,11 @@
 package com.reddit.clone.service;
 
 import com.reddit.clone.repository.EmployeeRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
 
