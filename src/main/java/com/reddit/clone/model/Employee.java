fac28cd407e518f18fe520fa74c53aeb4c2982b3
Artjom Makarov <armaka@ttu.ee>
2021-03-21 10:08:02 +0200

    Add @GeneratedValue to indicate HOW this field will be generated inside db

--- src/main/java/com/reddit/clone/model/Employee.java
+++ src/main/java/com/reddit/clone/model/Employee.java
@@ -1,12 +1,15 @@
 package com.reddit.clone.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
 @Entity
 public class Employee implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
