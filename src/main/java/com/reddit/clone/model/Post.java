6f3f846586e473772b5c3487dad17c0b2f40a3d4
Artjom Makarov <armaka@ttu.ee>
2021-03-21 09:09:22 +0200

    Add email filed and add add annotation that initializes that field as EMAIL, by validation also

--- src/main/java/com/reddit/clone/model/Post.java
+++ src/main/java/com/reddit/clone/model/Post.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Data
@@ -24,6 +25,7 @@ public class Post {
     private String postName;
     @NotBlank(message = "Password is required")
     private String password;
-
+    @Email
+    private String email;
 
 }
