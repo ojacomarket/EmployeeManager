84c77f59c413e3fd4ccb1ed010044c363dc3933f
Artjom Makarov <armaka@ttu.ee>
2021-03-21 14:42:55 +0200

    Annotation @GetMapping wihtou params will map its URL to @RequestMapping URL, then method getAllEmployees will be called, which return HTTP webpage with content of All employees --> list of employees

--- src/main/java/com/reddit/clone/controller/EmployeeController.java
+++ src/main/java/com/reddit/clone/controller/EmployeeController.java
@@ -1,14 +1,22 @@
 package com.reddit.clone.controller;
 
+import com.reddit.clone.model.Employee;
 import com.reddit.clone.service.EmployeeService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
 @AllArgsConstructor
 public class EmployeeController {
     private final EmployeeService employeeService;
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        
+    }
 }
