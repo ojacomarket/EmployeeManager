5ee91baefa1d8704c5eb42fd7a9909728c7f35d9
Artjom Makarov <armaka@ttu.ee>
2021-03-22 23:13:57 +0200

    Add method to switch between Modals and add variable that will create button on that rendered Modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,18 +25,6 @@ export class AppComponent implements OnInit{
     this.getEmployeesFromService();
   }
   public onOpenModal(employee: Employees, mode: string): void {
-    const containerBlock = document.getElementById('mainContainer');
     const button = document.createElement('button');
-    button.type = 'button';
-    button.style.display = 'none';
-    button.setAttribute('data-toggle', 'modal');
-    if (mode === 'add') {
-      button.setAttribute('data-target', '#addEmployeeModal');
-    }
-    if (mode === 'delete') {
-      button.setAttribute('data-target', '#deleteEmployeeModal');
-    }
-    containerBlock.appendChild(button);
-    button.click();
   }
 }
