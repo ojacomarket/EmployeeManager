db2798a23d74cc9197518836aa72ee4dc12225b5
Artjom Makarov <armaka@ttu.ee>
2021-03-22 16:35:43 +0200

    Fix Employee to Employees and delete const keyword

--- src/app/employee-service.service.ts
+++ src/app/employee-service.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {Employees} from './employees';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeServiceService {
-  private const BACKEND_SERVER_URL = '';
+  private BACKEND_SERVER_URL = '';
 
   constructor(private http: HttpClient) { }
 
-  public getEmployees(): Observable<Employee[]> {
-    return this.http.get<Employee[]>(`${this.BACKEND_SERVER_URL}/employee`);
+  public getEmployees(): Observable<Employees[]> {
+    return this.http.get<Employees[]>(`${this.BACKEND_SERVER_URL}/employee`);
   }
 }
