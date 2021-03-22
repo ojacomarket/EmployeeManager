a3e8aeedbbf27d9ffc247058ae09d87b71b71e06
Artjom Makarov <armaka@ttu.ee>
2021-03-22 23:14:24 +0200

    Add attributes to that button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,5 +26,18 @@ export class AppComponent implements OnInit{
   }
   public onOpenModal(employee: Employees, mode: string): void {
     const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode === 'edit') {
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    button.click();
   }
 }
