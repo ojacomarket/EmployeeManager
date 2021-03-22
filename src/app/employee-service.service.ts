1370489a396f2a4208b9368c2acee95c7d3fc47d
Artjom Makarov <armaka@ttu.ee>
2021-03-22 16:31:16 +0200

    Add GET request to backend Employees db

--- src/app/employee-service.service.ts
+++ src/app/employee-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +9,8 @@ export class EmployeeServiceService {
   private const BACKEND_SERVER_URL = '';
 
   constructor(private http: HttpClient) { }
+
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.BACKEND_SERVER_URL}/employee`);
+  }
 }
